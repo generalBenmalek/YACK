2c868abea85c38d5ef641c5d8b4709e536e1e309
aaayme <185227147+generalBenmalek@users.noreply.github.com>
2025-10-26 23:51:22 +0100

    feat: added routes, init for hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:yack/screens/auth/forgetPassword.dart';
+import 'package:yack/screens/auth/login.dart';
+import 'package:yack/screens/auth/signup.dart';
 import "../theme/theme.dart";
 import 'screens/settings.dart'; 
 import 'screens/create_contract.dart';
@@ -8,6 +12,7 @@ import 'package:hive/hive.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
@@ -21,7 +26,16 @@ class MyApp extends StatelessWidget {
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
-      home: const CreateContractScreen(), // Just to see the Settings Screen
+      routes: {
+        // '/': (context) => Home,
+        '/signup': (context) => const SignUpScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/settings': (context) => const SettingsScreen(),
+        '/reset_password': (context) => const ForgetPassword(),
+        '/contract/create': (context) => const CreateContractScreen()
+        // TODO: add routes for your page after finishing
+      },
+      initialRoute: '/signup' // will be changed later,
     );
   }
 }
