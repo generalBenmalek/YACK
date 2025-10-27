a532f1d64b93d454f6bd91e5d5228dfc6d74e765
aaayme <185227147+generalBenmalek@users.noreply.github.com>
2025-10-27 18:21:13 +0100

    feat: FIREBASE,
    added firebase along with init
    added firstTime, returning dynamic root page

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,47 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:yack/screens/auth/forgetPassword.dart';
 import 'package:yack/screens/auth/login.dart';
 import 'package:yack/screens/auth/signup.dart';
-import "../theme/theme.dart";
-import 'screens/settings.dart'; 
-import 'screens/sign_contract.dart';
+import '../theme/theme.dart';
+import 'screens/settings.dart';
 import 'screens/create_contract.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:hive/hive.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Hive first
   await Hive.initFlutter();
-  runApp(const MyApp());
+
+  // Open user box
+  final userBox = await Hive.openBox('user');
+
+  late final String initialRoute;
+
+  // Determine first-time or returning user
+  if (userBox.get('firstTime') == null || userBox.get('firstTime') == false) {
+    initialRoute = '/signup'; // later replaced with welcome screen
+    await userBox.put('firstTime', true);
+  } else if (userBox.get('didFirstLogin') == true) {
+    initialRoute = '/login';
+  } else {
+    initialRoute = '/signup';
+  }
+
+  // Initialize Firebase
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(MyApp(initialRoute: initialRoute));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
+  final String initialRoute;
+  const MyApp({super.key, required this.initialRoute});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,15 +50,13 @@ class MyApp extends StatelessWidget {
       darkTheme: AppTheme.darkTheme,
       themeMode: ThemeMode.system,
       routes: {
-        // '/': (context) => Home,
         '/signup': (context) => const SignUpScreen(),
         '/login': (context) => const LoginScreen(),
         '/settings': (context) => const SettingsScreen(),
         '/reset_password': (context) => const ForgetPassword(),
-        '/contract/create': (context) => const CreateContractScreen()
-        // TODO: add routes for your page after finishing
+        '/contract/create': (context) => const CreateContractScreen(),
       },
-      initialRoute: '/signup' // will be changed later,
+      initialRoute: initialRoute, // dynamic\
     );
   }
 }
